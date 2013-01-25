9f95b491c111cd6282c5b8a344e921a4d1ce8c31
Régis Behmo <regis@behmo.com>
2013-01-25 18:10:45 +0100

    Working pdf form generation and download

--- tools/views.py
+++ tools/views.py
@@ -1,19 +1,23 @@
 from django.http import HttpResponse
+from django.core.servers.basehttp import FileWrapper
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django import forms
+from StringIO import StringIO
+import tempfile, shutil, os
 
 class RegisterForm(forms.Form):
-  first_name = forms.CharField(min_length=10)
-  last_name = forms.CharField(max_length=50)
+  firstname = forms.CharField(max_length=50)
+  lastname = forms.CharField(max_length=50)
   email = forms.EmailField()
-  phone_number = forms.CharField(max_length=20)
+  mobile = forms.CharField(max_length=20)
   gender = forms.ChoiceField(choices=[("male", "Male"), ("female", "Female")])
   birthday = forms.DateField()
   experience = forms.IntegerField()
-  address1 = forms.CharField()
-  address2 = forms.CharField()
-  address3 = forms.CharField()
+  addresslineone = forms.CharField()
+  addresslinetwo = forms.CharField()
+  cityandpostcode = forms.CharField()
+  img = forms.FileField()
 
 def get_form(request):
   if request.method == "GET":
@@ -23,12 +27,52 @@ def get_form(request):
                               context_instance=RequestContext(request))
 
   # Process form
-  form = RegisterForm(request.POST)
+  form = RegisterForm(request.POST, request.FILES)
   if not form.is_valid():
-    print "form is not valid", form.errors
     return render_to_response("register_form_response.html",
                               {"form": form},
                               context_instance=RequestContext(request))
 
-  return HttpResponse("blah!")
+  data_dir = os.path.join(os.path.dirname(__file__), "..", "data")
+  # Do all the processing in a temporary folder
+  tempdir = tempfile.mkdtemp()
+  print "doing everything in", tempdir
+  # Save image to temporary file
+  img_name = request.FILES["img"].name
+  img_content = request.FILES["img"].read()# TODO read chunk by chunk
+  img_orig = tempfile.NamedTemporaryFile(dir=tempdir, delete=False)
+  print "temp image file:", img_orig.name
+  img_orig.write(img_content)
+  img_orig.flush()
+  # Convert image to png format
+  img_dst = os.path.join(tempdir, "photo.png")
+  os.system("convert %s %s" % (img_orig.name, img_dst))
+  # Load tex data and process it
+  data_src = os.path.join(data_dir, "lions.tex")
+  with open(data_src) as f:
+    data = f.read()
+    for field in form:
+      if field.name != "img":
+        data = data.replace("\\" + field.name, field.data)
+      else:
+        data = data.replace("\\photoid", "photo.png")
+    tex_dst = os.path.join(tempdir, "lions.tex")
+    # Write tex data to temp file
+    with open(tex_dst, "w") as tex_file:
+      tex_file.write(data)
+  # Copy lvb.png and lions.png image
+  shutil.copy(os.path.join(data_dir, "lions.png"), os.path.join(tempdir, "lions.png"))
+  shutil.copy(os.path.join(data_dir, "lvb.png"), os.path.join(tempdir, "lvb.png"))
+  # Compile
+  os.system("cd %s && pdflatex -halt-on-error lions.tex" % tempdir)
+  
+  # Return pdf file
+  response = HttpResponse(open(os.path.join(tempdir, "lions.pdf")).read(),
+                          content_type='application/pdf')
+  response['Content-Disposition'] = 'attachment; filename=lions_register.pdf'
 
+  # Delete temporary directory
+  shutil.rmtree(tempdir)
+  print "directory tree deleted"
+
+  return response
