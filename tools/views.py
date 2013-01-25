28b01871747880b3e6d93f877402dd4111c8380b
Régis Behmo <regis@behmo.com>
2013-01-25 15:48:46 +0100

    Use django form and handle errors

--- tools/views.py
+++ tools/views.py
@@ -1,26 +1,34 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django import forms
+
+class RegisterForm(forms.Form):
+  first_name = forms.CharField(min_length=10)
+  last_name = forms.CharField(max_length=50)
+  email = forms.EmailField()
+  phone_number = forms.CharField(max_length=20)
+  gender = forms.ChoiceField(choices=[("male", "Male"), ("female", "Female")])
+  birthday = forms.DateField()
+  experience = forms.IntegerField()
+  address1 = forms.CharField()
+  address2 = forms.CharField()
+  address3 = forms.CharField()
 
 def get_form(request):
   if request.method == "GET":
-    print "Request method is GET", request.method
+    form = RegisterForm()
     return render_to_response("register_form.html",
+                              {"form": form},
                               context_instance=RequestContext(request))
-  print "Request method is not GET", request.method
 
   # Process form
-  first_name = request.POST["first_name"]
-  last_name = request.POST["last_name"]
-  email = request.POST["email"]
-  phone_number = request.POST["phone_number"]
-  gender = request.POST["gender"]
-  birthday = request.POST["birthday"]
-  experience = request.POST["experience"]
-  address1 = request.POST["address1"]
-  address2 = request.POST["address2"]
-  address3 = request.POST["address3"]
+  form = RegisterForm(request.POST)
+  if not form.is_valid():
+    print "form is not valid", form.errors
+    return render_to_response("register_form_response.html",
+                              {"form": form},
+                              context_instance=RequestContext(request))
 
-  return render_to_response("register_form_response.html",
-                            context_instance=RequestContext(request))
+  return HttpResponse("blah!")
 
