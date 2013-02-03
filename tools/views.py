e380220a930d3a47bf58df6d0b26daee6f3627d6
Nazcafan <alex.m.fournier@gmx.com>
2013-02-03 12:01:07 +0000

    minor fixes and licencing
    -fixed male/female checkbox
    -set birthday to Europe format dd/mm/yy
    -added licence on files I worked on

--- tools/views.py
+++ tools/views.py
@@ -1,4 +1,22 @@
 # -*- coding: utf-8 -*-
+# 
+# copyright Régis Behmo and Alexandre Fournier, distributed under the 
+# following terms:
+# 
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                    Version 2, December 2004
+# 
+# Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+# 
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+# 
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+# 
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+
 from django.http import HttpResponse
 from django.core.servers.basehttp import FileWrapper
 from django.shortcuts import render_to_response
